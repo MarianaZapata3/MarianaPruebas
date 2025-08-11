36f6c3d80ce51ea268bbc4e7283ee01ebee559a4
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-08-10 21:12:41 -0300

    Agregar lo que avance

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+print("Mariana")
+print(not((5==4) and (4==3)))
+lasuma = 1
+print(not(lasuma == 2))
+print(lasuma + 5)
